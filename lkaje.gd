9922fee122e2b9777e8a061537418075afaa5592
José Pedro <jose.pedro.fagundes.gadelha@gmail.com>
2026-09-23 14:19:56 -0300

    cammit 3

---
+++ lkaje.gd
@@ -0,0 +1,19 @@
+extends Sprite2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	#st hgsbyt
+	#tysert
+	#hyewrtynertun
+	#er
+	#uner
+	#un
+	#etry
+	#ue
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
